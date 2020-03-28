0164b619c6f28ccb0807c219635abfb27ffdf5ca
Leandro Ostera <leandro@ostera.io>
2020-03-28 18:09:44 +0100

    [scratchpad] Fix mouse coords to use new caching and react to cursor_moved events

--- apps/scratchpad/src/mouse_coords.erl
+++ apps/scratchpad/src/mouse_coords.erl
@@ -12,6 +12,7 @@
 -export([ draw/0
         , start/0
         , restart/0
+        , handle_event/1
         ]).
 
 %%==============================================================================
@@ -22,19 +23,18 @@ start_link(Args, Opts) ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, Args, Opts).
 
 init(Args) ->
+  process_flag(priority, high),
   State = initial_state(Args),
-  Self = self(),
-  chalk_event_server:register(Self),
-  chalk_pipeline:register(fun () -> mouse_coords:draw() end),
+  chalk_event_server:register(fun ?MODULE:handle_event/1),
+  chalk_pipeline:register(fun mouse_coords:draw/0),
   {ok, State}.
 
 terminate(_, _) -> ok.
 
+handle_call({move_cursor, Pos, Ts}, _From, State) -> do_cursor_move(Pos, Ts, State);
 handle_call(draw, _From, State) -> do_draw(State);
 handle_call(_Msg, _From, State) -> {noreply, State}.
 
-handle_cast({{type, cursor_moved}, [{x,X}, {y,Y}]}, State) ->
-  {noreply, do_cursor_move(State, {X,Y})};
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
@@ -42,16 +42,16 @@ handle_cast(_Msg, State) ->
 %% Api
 %%==============================================================================
 
-start() ->
-  mouse_coords:start_link([],[]),
-  chalk_pipeline:flush().
+start() -> mouse_coords:start_link([],[]).
 
 restart() ->
   gen_server:stop(mouse_coords),
-  chalk_pipeline:clear(),
   start().
 
 draw() -> gen_server:call(?MODULE, draw).
+
+handle_event(Event={Ts, {{type, cursor_moved}, [{x,X}, {y,Y}]}}) ->
+  gen_server:call(?MODULE, {move_cursor, {X,Y}, Ts}).
 
 %%==============================================================================
 %% Internal
@@ -60,16 +60,24 @@ draw() -> gen_server:call(?MODULE, draw).
 initial_state(_) ->
   #{ pos => {1000.0, 1000.0, 10.0}
    , time => erlang:system_time()
+   , last_event_time => 0
+   , crosshair => crosshair({1000.0, 1000.0, 10.0})
    }.
 
-do_cursor_move(State, {X, Y}) -> State#{ pos => {X * 1.0, Y * 1.0, 1.0} }.
+do_cursor_move({X, Y}, Ts, State) ->
+  %io:format("mouse_coords:do_cursor_move/3:\t\t~pms (since event ocurred)\n\n", [(erlang:system_time() - Ts)/1000000]),
+  {reply, ok, State#{ pos => {X * 1.0, Y * 1.0, 1.0}
+                    , last_event_time => Ts
+                    , crosshair => crosshair({X, Y, 0.0})
+                    }}.
 
-do_draw(#{ pos := {X,Y,Z} = Pos
-         , time := _
+do_draw(#{ pos := {X,Y,Z} = _Pos
+         , time := _LastRenderTime
+         , last_event_time := _Ts
+         , crosshair := Crosshair
          }=State) ->
   Now = erlang:system_time(),
-  Crosshair = crosshair(Pos),
-  {reply, {ok, {X - 50.0, Y - 50.0, Z}, Crosshair}, State#{ time => Now }}.
+  {reply, {new_frame, {X - 50.0, Y - 50.0, Z}, Crosshair}, State#{ time => Now }}.
 
 
 crosshair({X, Y, _}) ->
@@ -83,7 +91,7 @@ crosshair({X, Y, _}) ->
   sk_path:line_to(P, 100.0, 50.0),
   sk_canvas:draw_path(C, P, red_paint()),
 
-  Font = sk_font:new([{size, 30.0}]),
+  Font = sk_font:default(),
   Text = lists:flatten(io_lib:format("(~p,~p)", [round(X), round(Y)])),
   TextBlob = sk_text_blob:from_binary(Text, Font),
   sk_canvas:draw_text_blob(C, TextBlob, 50, 75, black_paint()),

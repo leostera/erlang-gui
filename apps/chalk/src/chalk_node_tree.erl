394373b66f06656e694fd547d6ce87fd486ea8da
Leandro Ostera <leandro@ostera.io>
2020-04-08 10:18:12 +0200

    [chalk] Uses Quadtree for parallelizing renders
    This refactor swaps the original linear render table with a partitioned
    table that follows a Quadtree mapping.
    
    What this wins is the ability to parallelize across the Z, Y, and X axis
    in __quads__, where each partition of the table can be folded into a
    single picture, concurrently, and if possible, parallelly.

--- apps/chalk/src/chalk_node_tree.erl
+++ apps/chalk/src/chalk_node_tree.erl
@@ -1,70 +1,198 @@
 -module(chalk_node_tree).
 
--compile([export_all]).
+-behaviour(gen_server).
 
-new() -> setup().
+-export([ start_link/2
+        , stop/0
+        , init/1
+        , terminate/2
+        , handle_call/3
+        , handle_cast/2
+        ]).
 
-default_table_opts() ->
-  [ named_table
-  , ordered_set
-  , {read_concurrency, true}
-  , {write_concurrency, true}
-  , public
-  ].
+-export([ clear/0
+        , dump/0
+        , get/1
+        , add/1
+        , foreach/1
+        , foreach/2
+        , size/0
+        , rect/1
+        , rect/2
+        ]).
 
-setup() ->
-  Opts = default_table_opts(),
-  Cache = cets:new(2, <<"chalk_render_tree_cache">>, Opts),
-  Refs = cets:new(16, <<"chalk_render_tree_ref_table">>, Opts),
-  Zs = ets:new(chalk_render_tree_flush_table, Opts),
+
+%%==============================================================================
+%% Behavior callbacks
+%%==============================================================================
+
+init(_Args) ->
+  process_flag(priority, high),
+  {ok, initial_state()}.
+
+terminate(_, _) -> ok.
+
+handle_call({foreach, Fn}, _From, State) -> do_foreach(Fn, State);
+handle_call({add, Node}, _From, State) -> do_add(Node, State);
+handle_call({get, Key}, _From, State) -> do_get(Key, State);
+handle_call({get_rect, Node}, _From, State) -> do_get_rect(Node, State);
+handle_call(size, _From, State) -> do_size(State);
+handle_call(clear, _From, State) -> do_clear(State);
+handle_call(dump, _From, State) -> {reply, State, State};
+handle_call(_, _From, State) -> {noreply, State}.
+
+handle_cast(_Msg, State) -> {noreply, State}.
+
+
+%%==============================================================================
+%% Api
+%%==============================================================================
+
+start_link(Args, Opts) -> gen_server:start_link({local, ?MODULE}, ?MODULE, Args, Opts).
+
+stop() -> gen_server:stop(?MODULE).
+
+clear() -> gen_server:call(?MODULE, clear).
+
+dump() -> gen_server:call(?MODULE, dump).
+
+get(Key) -> gen_server:call(?MODULE, {get, Key}).
+
+rect(Node) -> gen_server:call(?MODULE, {get_rect, Node}).
+
+rect(Node, State) ->
+  {reply, Rect, _} = do_get_rect(Node, State),
+  Rect.
+
+add(Node) when is_function(Node) -> gen_server:call(?MODULE, {add, Node}).
+
+foreach(Fn) when is_function(Fn) -> gen_server:call(?MODULE, {foreach, Fn}).
+
+foreach(Fn, State) when is_function(Fn) ->
+  {reply, ok, _} = do_foreach(Fn, State),
+  ok.
+
+size() -> gen_server:call(?MODULE, size).
+
+%%==============================================================================
+%% Internal
+%%==============================================================================
+
+initial_state() ->
+  Dimensions = {500, 1920, 1080},
+  Cache = cets:new(<<"chalk_render_tree_cache">>),
+  Refs = cets:new(<<"chalk_render_tree_ref_table">>),
+  % Try out the octree
+  Octree = eu_octree:new({500, 1920, 1080}),
+  Z0 = cets:new(<<"chalk_render_tree_flush_table">>),
+  Zs = cets:set_partitioning_function(
+         fun ({Z, Y, X, _}, Partitions) ->
+             Point = {Z,Y,X},
+             case eu_octree:member(Point, Octree) of
+               {hit, {Idx0, Idx1}}  ->
+                 PartSize = maps:size(Partitions),
+                 round(math:fmod((Idx0+1 * Idx1+1),PartSize));
+               _ -> 0
+             end
+         end, Z0),
   #{ refs => Refs
    , cache => Cache
    , render => Zs
+   , octree => Octree
+   , dimensions => Dimensions
    }.
 
-clear(#{ refs := RefTable
-       , cache := Cache
-       , render := ZsTable }) ->
+
+do_clear(#{ refs := RefTable
+          , cache := Cache
+          , render := ZsTable }) ->
   cets:delete_all_objects(RefTable),
   cets:delete_all_objects(Cache),
-  ets:delete_all_objects(ZsTable),
+  cets:delete_all_objects(ZsTable),
   ok.
 
-get(Ref, #{ refs := RefTable }) ->
-  case cets:lookup(RefTable, Ref) of
+do_get(Ref, #{ refs := RefTable }=State) ->
+  Result = case cets:lookup(RefTable, Ref) of
     [] -> none;
     [V] -> V
+  end,
+  {reply, Result, State}.
+
+do_get_rect(Ref, #{ cache := Cache }=State) ->
+  case cets:lookup(Cache, Ref) of
+    [] ->
+      {reply, {error, rect_is_yet_unknown}, State};
+    [{Ref, {{X, Y, _}, {W, H}, _Pict}}] ->
+      {reply, eu_rect:make(X, Y, W, H), State}
   end.
 
-add(Fn, #{ refs := RefTable }) ->
+
+do_add(Fn, #{ refs := RefTable }=State) ->
   Ref = id(),
   true = cets:insert(RefTable, {Ref, Fn}),
-  {ok, Ref}.
+  {reply, {ok, Ref}, State}.
 
-fold(F, #{ render := ZsTable, refs := RefTable, cache := Cache }) ->
-  cets:foldl(fun
-               ({Ref, Fn}, _) ->
-                 (catch (case Fn() of
-                           cached ->
-                             [{_, {LastPos, LastPic}}] = cets:lookup(Cache, Ref),
-                             ets:insert(ZsTable, {key(LastPos, Ref), LastPic});
+do_foreach(F, #{ render := ZsTable
+               , refs := RefTable
+               , octree := Octree
+               , cache := Cache }=State) ->
+  {T, _} = timer:tc(fun () ->
+  cets:foldl(
+    fun ({Ref, Fn}, _) ->
+        case (catch Fn()) of
+          cached -> queue_cached_frame(Ref, Cache, ZsTable);
+          {new_frame, _, _, _}=Frame -> queue_new_frame(Ref, Frame, State);
+          _ -> ok
+          end
+    end, ok, RefTable)
+  end),
+  io:format("build z table: ~pμs\n", [T]),
+  {T1, Pictures} =
+    timer:tc(fun () ->
+      cets:fold(fun ({{Z, Y, X, _}, Pic}, {_, C}) ->
+                     sk_canvas:draw_picture_at(C, {X, Y}, Pic),
+                     {{Z, Y, X}, C}
+                end,
+                fun () -> {first, sk_canvas:new(1920, 1080)} end,
+                ZsTable)
+    end),
+  io:format("z octants draw: ~pμs\n", [T1]),
+  Frame = lists:foldl(fun ({I, {Pos, TempC}}, C) ->
+                          Idx = eu_octree:linear_idx_to_pair(I),
+                          {_, {X,_}, {Y,_}} = eu_octree:limits(Idx, Octree),
+                          Pic = sk_picture:from_canvas(TempC),
+                          sk_canvas:draw_picture_at(C, {X*1.0, Y*1.0}, Pic),
+                          C;
+                          (_, C) -> C
+                      end,
+                      sk_canvas:new(1920, 1080),
+                      lists:sort(fun ({A, _}, {B, _}) -> A < B end, Pictures)),
+  F(sk_picture:from_canvas(Frame)),
+  cets:delete_all_objects(ZsTable),
+  {reply, ok, State}.
 
-                           {new_frame, NewPos, NewPic} ->
-                             KV = {key(NewPos, Ref), NewPic},
-                             cets:insert(Cache, {Ref, {NewPos, NewPic}}),
-                             ets:insert(ZsTable, KV),
-                             cets:insert(RefTable, {Ref, Fn});
+queue_cached_frame(Ref, Cache, ZsTable) ->
+  [{_, {LastPos, _LastDims, LastPic}}] = cets:lookup(Cache, Ref),
+  cets:insert(ZsTable, {key(LastPos, Ref), LastPic}).
 
-                           _ -> ok
-                         end))
-            end, none, RefTable),
-  ets:foldl(fun ({K,V}, Acc) -> F({unkey(K),V}), Acc end, none, ZsTable),
-  ets:delete_all_objects(ZsTable),
-  ok.
+queue_new_frame( Ref
+               , {new_frame, {X, Y, Z}=NewPos, {W, H}, NewPic}
+               , #{ render := ZsTable, cache := Cache, octree := Oct }) ->
+  case eu_octree:member({Z, Y, X}, Oct) of
+    {hit, _} -> NewDims = { W*1.0, H*1.0 },
+                cets:insert(Cache, {Ref, {NewPos, NewDims, NewPic}}),
+                cets:insert(ZsTable, {key(NewPos, Ref), NewPic});
+    _ -> ok
+  end.
+
+
+
+do_size(#{ refs := RefTable }=State) ->
+  {reply, {ok, ets:info(RefTable, size)}, State}.
+
+%%==============================================================================
+%% Utilities
+%%==============================================================================
 
 id() -> make_ref().
-
-key({X,Y,Z}, Ref) -> {Z, Y, X, Ref}.
-unkey({Z,Y,X,_}) -> {X,Y,Z}.
-
-size(#{ refs := RefTable }) -> ets:info(RefTable, size).
+key({X,Y,Z}, _Ref) -> {Z, Y, X, erlang:system_time()}.

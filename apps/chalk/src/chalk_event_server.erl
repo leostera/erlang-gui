47b578f787ab1d11530cd75b0d9cd087390380d3
Leandro Ostera <leandro@ostera.io>
2020-04-08 10:11:54 +0200

    [chalk] Split registration: event and node based
    Before this, registration happened on all events, all of the time. After
    this, registration for events can happen on an event kind basis, or on a
    node-overlap.
    
    The first one is useful when only a subset of all events are relevant:
    e.g, if only key-presses are useful, you can register to listen to only
    those. If you do want all events, you can register without specifyin
    which event kind you are interested in.
    
    The second kind allows you to register events that are spatially
    relevant to a particular node in the `chalk_node_tree`. It's a common
    yet specific case, where events like `cursor_moved`, can be relevant
    only if the coordinates of the mouse are overlapping the coordinates of
    your element node.
    
    In any case, if the callback to the event crashes, it will be removed.

--- apps/chalk/src/chalk_event_server.erl
+++ apps/chalk/src/chalk_event_server.erl
@@ -1,5 +1,7 @@
 -module(chalk_event_server).
 
+-compile({no_auto_import,[register/1, register/2]}).
+
 -behavior(gen_server).
 
 -export([ start_link/2
@@ -14,6 +16,7 @@
 -export([ send/1
         , dump/0
         , register/1
+        , register/2
         ]).
 
 %%==============================================================================
@@ -37,7 +40,8 @@ handle_cast({send, Event}, State) ->
   {noreply, NewState};
 handle_cast(_Msg, State) -> {noreply, State}.
 
-handle_call({register, H}, _From, State) -> do_register(H, State);
+handle_call({register_matcher, M}, _From, State) -> do_register_matcher(M, State);
+handle_call({register_node, M}, _From, State) -> do_register_node(M, State);
 handle_call(dump, _From, State) -> {reply, State, State};
 handle_call(_Msg, _From, State) -> {noreply, State}.
 
@@ -51,20 +55,31 @@ dump() -> gen_server:call(?MODULE, dump).
 
 send(Event) -> gen_server:cast(?MODULE, {send, Event}).
 
-register(F) when is_function(F) -> gen_server:call(?MODULE, {register, {self(), F}}).
+register(F) when is_function(F) -> register(all, F).
+
+register(EventType, F) when is_function(F) and is_atom(EventType) ->
+  true = chalk_event:is_valid_type(EventType),
+  gen_server:call(?MODULE, {register_matcher, {self(), EventType, F}}, infinity);
+
+register(NodeRef, F) when is_function(F) and is_reference(NodeRef) ->
+  gen_server:call(?MODULE, {register_node, {self(), NodeRef, F}}, infinity).
 
 %%==============================================================================
 %% Internal
 %%==============================================================================
 
 initial_state() ->
-  #{ handlers => cets:new( 2
-                         , <<"chalk_event_server">>
-                         , [{write_concurrency, true}, {read_concurrency, true}, ordered_set])
+  #{ handlers_by_event => cets:new(2, <<"chalk_event_server_handlers_by_event_table">>)
+   , handlers_by_node => cets:new(2, <<"chalk_event_server_handlers_by_node_table">>)
+   , node_tree => chalk_node_tree:dump()
    }.
 
-do_register(Handler, #{ handlers := Table }=State) ->
-  cets:insert(Table, Handler),
+do_register_matcher({Pid, EventName, Fn}, #{ handlers_by_event := Table }=State) ->
+  cets:insert(Table, {{EventName, Pid}, Fn}),
+  {reply, ok, State}.
+
+do_register_node({Pid, NodeRef, Fn}, #{ handlers_by_node := Table }=State) ->
+  cets:insert(Table, {{NodeRef, Pid}, Fn}),
   {reply, ok, State}.
 
 do_handle_event(Event={_Ts, {{type, resized}, [{w,W},{h,H}]}}, State) ->
@@ -87,19 +102,61 @@ do_handle_event(Event, State) ->
 %% Utilities
 %%==============================================================================
 
-fanout(Event, #{ handlers := Table }) ->
-  cets:foldl(fun ({Pid, Handler}, _) ->
-                 spawn(fun () ->
-                           case is_process_alive(Pid) of
-                             true ->
-                               case (catch Handler(Event)) of
-                                 {'EXIT', _} -> cets:delete(Table, Pid);
-                                 _ -> ok
-                               end;
-                             false -> cets:delete(Table, Pid)
-                           end
-                       end)
-             end, ok, Table).
+fanout(Event, State) ->
+  case chalk_event:has_position(Event) of
+    true -> fanout_by_node(Event, State);
+    _ -> ok
+  end,
+  fanout_by_event(Event, State).
+
+fanout_by_event(Event, #{ handlers_by_event := Table }) ->
+  EvType = chalk_event:type(Event),
+  cets:foldl(
+    fun ({Key={RegEvType, Pid}, Handler}, _)  when (RegEvType =:= EvType )
+                                                or (RegEvType =:= all)   ->
+        Alive = is_process_alive(Pid),
+        Result = case Alive of
+                   true -> call_handler(Event, Handler);
+                   false -> remove_handler(Key, Table)
+                 end,
+        case Result of
+          {error, _Reason} -> remove_handler(Key, Table);
+          _ -> ok
+        end;
+        (_, _) -> ok
+    end, ok, Table).
+
+fanout_by_node(Event, #{ handlers_by_node := NodeTable, node_tree := Nodes }) ->
+  {ok, Position} = chalk_event:position(Event),
+  cets:foldl(
+    fun ({Key={Node, Pid}, Handler}, _) ->
+        case chalk_node_tree:rect(Node, Nodes) of
+          {ok, Rect} ->
+            Hit = eu_rect:is_point_within(Rect, Position),
+            Alive = is_process_alive(Pid),
+            Result = case {Alive, Hit} of
+                       {true, true} -> call_handler(Event, Handler);
+                       {false, _} -> remove_handler(Key, NodeTable);
+                       _  -> handler_ignored
+                     end,
+            case Result of
+              {error, _Reason} -> remove_handler(Key, NodeTable);
+              _ -> ok
+            end;
+          _ ->
+            node_not_yet_rendered
+        end
+    end, ok, NodeTable).
+
+call_handler(Event, Handler) ->
+  case (catch Handler(Event)) of
+    {'EXIT', Reason} -> {error, Reason};
+    _ -> ok
+  end.
+
+remove_handler(Key, Table) ->
+  ok = cets:delete(Table, Key),
+  ok.
 
 
 scale_coords({W, H}, {X, Y}) ->
